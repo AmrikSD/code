b2a4c3d26d72d355a905a3eb910b86d259c33383
AmrikSD <amrik.singh@live.co.uk>
2024-05-03 18:46:47 +0100

    remove jobs specific rules, catchall works well

--- infra/cloudflare/02-email.tf
+++ infra/cloudflare/02-email.tf
@@ -25,35 +25,3 @@ resource "cloudflare_email_routing_catch_all" "catchall" {
   }
 
 }
-
-
-
-### Jobs
-
-resource "cloudflare_email_routing_address" "jobs" {
-  account_id = data.sops_file.cloudflare-secret.data["cloudflare.account_id"]
-  email      = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.hire_email"]
-  lifecycle {
-    ignore_changes = [
-      account_id
-    ]
-  }
-}
-
-resource "cloudflare_email_routing_rule" "jobs" {
-  zone_id = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.zone_id"]
-  name    = "Jobs - Managed By Terraform"
-  enabled = true
-
-  matcher {
-    type  = "literal"
-    field = "to"
-    value = "hire@amrik.co.uk"
-  }
-
-  action {
-    type  = "forward"
-    value = [cloudflare_email_routing_address.jobs.email]
-  }
-
-}
