99d66c374d366cf683e910cbfe4a761906ce4abc
AmrikSD <amrik.singh@live.co.uk>
2024-05-10 05:38:35 +0100

    bazel

---
+++ projects/Leetcode/src/main/java/uk/co/amrik/leetcode/Leetcode.java
@@ -0,0 +1,7 @@
+package uk.co.amrik.leetcode;
+
+public class Leetcode {
+    public static void main (String ...args){
+        System.out.println("Hello, world!");
+    }
+}
---
+++ projects/Leetcode/src/test/java/uk/co/amrik/leetcode/LeetcodeTest.java
@@ -0,0 +1,14 @@
+package uk.co.amrik.leetcode;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LeetcodeTest {
+
+    @Test
+    public void shouldAlwaysPass(){
+        assertEquals(2+2, 4);
+    }
+
+}
