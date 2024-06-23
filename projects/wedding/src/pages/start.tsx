ccbb13177e77fbdafdbf5d4adaec834d041f63ba
AmrikSD <amrik.singh@live.co.uk>
2024-06-23 02:09:15 +0100

    small styling

--- projects/wedding/src/pages/start.tsx
+++ projects/wedding/src/pages/start.tsx
@@ -2,13 +2,21 @@ import Countdown from "@/app/Countdown"
 import React from "react"
 
 export default function Start() {
-    return <>
-        <h2>We are getting married!</h2>
-        <Countdown target={new Date("2025-02-02")} />
-        <p>You&apos;re invited!</p>
-        <p>Scroll down for some <a href="#info">more info</a>!</p>
-        <p><a href="#location">location</a></p>
-        <p><a href="#rsvp">rsvp</a></p>
-        <p><a href="#faq">faq</a></p>
-    </>
+    return (
+            <>
+                <div>
+                    <h1>Kate &amp; Amrik</h1>
+                    <h2>Are getting married</h2>
+                </div>
+                <Countdown target={new Date("2025-02-02")} />
+                <a href="#rsvp">
+                    <button>Don&apos;t forget to RSVP.</button>
+                </a>
+                <div>
+                    <p>Scroll down for some <a href="#info">more info</a>!</p>
+                    <p><a href="#location">location</a></p>
+                    <p><a href="#faq">faq</a></p>
+                </div>
+            </>
+           )
 }
