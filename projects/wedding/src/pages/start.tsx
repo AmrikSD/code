8f8759230957a43f6fd3ced55b693e94eb818411
AmrikSD <amrik.singh@live.co.uk>
2024-06-26 22:17:02 +0100

    css bits

--- projects/wedding/src/pages/start.tsx
+++ projects/wedding/src/pages/start.tsx
@@ -1,5 +1,6 @@
 import Countdown from "@/app/Countdown"
 import React from "react"
+import styles from "./start.module.css"
 
 export default function Start() {
     return (
@@ -9,9 +10,9 @@ export default function Start() {
                     <h2>Are getting married</h2>
                 </div>
                 <Countdown target={new Date("2025-02-02")} />
-                <a href="#rsvp">
-                    <button>Don&apos;t forget to RSVP.</button>
-                </a>
+                <div className={styles.btnContainer}>
+                    <a href="#rsvp" className={styles.btn}>Don&apos;t forget to RSVP.</a>
+                </div>
                 <div>
                     <p>Scroll down for some <a href="#info">more info</a>!</p>
                     <p><a href="#location">location</a></p>
