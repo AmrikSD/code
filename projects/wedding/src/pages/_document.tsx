9f90bbfcfcff180a8c3d304b6716a7629a691461
Amrik Singh <amrik.singh@starlingbank.com>
2024-06-27 09:50:56 +0100

    some styling

--- projects/wedding/src/pages/_document.tsx
+++ projects/wedding/src/pages/_document.tsx
@@ -3,7 +3,22 @@ import { Html, Head, Main, NextScript} from "next/document";
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
+                    rel="stylesheet" />
+
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap"
+                    rel="stylesheet" />
+
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+                    rel="stylesheet" />
+
+            </Head>
             <body>
                 <Main />
                 <NextScript />
