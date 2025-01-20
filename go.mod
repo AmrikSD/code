1856455a1cb22012c1dda287740af626bb8e465f
Amrik Singh <amrik.singh@starlingbank.com>
2025-01-20 16:00:48 +0000

    just list some files for now

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/amriksd/code
 
-go 1.22.0
+go 1.23.0
 
 require github.com/labstack/echo/v4 v4.13.3
 
--- projects/go-docs/cmd/main.go
+++ projects/go-docs/cmd/main.go
@@ -5,6 +5,10 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -18,22 +22,58 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+
+type File struct {
+    Name string
+    Size int64
+    Edited time.Time
+    Path string
+}
 func main(){
 
     e := echo.New()
     e.Static("/static", "public/static")
+
+    e.Static("/original", "input") //TODO Make this configurable
+
     e.Renderer = &Template{
         templates: template.Must(template.ParseGlob("public/views/*.html")),
+    }
+
+    filesList := make([]File, 0)
+    err := filepath.Walk("./input", func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+
+        if info.Mode().IsRegular() {
+            filesList = append(filesList, File{
+                info.Name(),
+                info.Size(),
+                info.ModTime(),
+                strings.TrimPrefix(path, "input/"),
+            })
+
+        }
+        return nil
+    })
+
+    if err != nil {
+        panic(err)
     }
 
     e.GET("/", func(c echo.Context) error {
         return c.Render(http.StatusOK, "index", nil)
     })
 
-    filesList := []string{"hil", "ho"}
 
     e.GET("/files", func(c echo.Context) error {
         return c.Render(http.StatusOK, "files", filesList)
+    })
+
+    e.GET("/modal/:file", func(c echo.Context) error {
+        f := c.Param("file")
+        return c.Render(http.StatusOK, "modal", f)
     })
 
 
