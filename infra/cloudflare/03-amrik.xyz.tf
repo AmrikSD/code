cfa7453d28d1a00ba2162a7cd95a5955a17c5b12
AmrikSD <amrik.singh@live.co.uk>
2025-06-06 01:23:30 +0100

    no more test subdomain

--- infra/cloudflare/03-amrik.xyz.tf
+++ infra/cloudflare/03-amrik.xyz.tf
@@ -15,13 +15,6 @@ resource "cloudflare_record" "amrik_xyz" {
   content = var.ip_address
 }
 
-resource "cloudflare_record" "test_amrik_xyz" {
-  zone_id = data.sops_file.cloudflare-secret.data["cloudflare.amrik.xyz.zone_id"]
-  name    = "test"
-  type    = "A"
-  content = var.ip_address
-}
-
 resource "cloudflare_zero_trust_tunnel_cloudflared" "gcp_tunnel" {
   account_id = data.sops_file.cloudflare-secret.data["cloudflare.account_id"]
   name       = "Frappe tunnel - terraform"
