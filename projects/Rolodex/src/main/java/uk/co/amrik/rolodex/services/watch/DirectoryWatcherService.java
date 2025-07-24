8c7973b20e7a3523bd30bd2a3be1fcbbf6b9bf16
AmrikSD <amrik.singh@live.co.uk>
2025-07-24 06:52:29 +0100

    walk the directory

--- projects/Rolodex/src/main/java/uk/co/amrik/rolodex/services/watch/DirectoryWatcherService.java
+++ projects/Rolodex/src/main/java/uk/co/amrik/rolodex/services/watch/DirectoryWatcherService.java
@@ -5,10 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.co.amrik.rolodex.services.BackgroundService;
 
-import java.lang.module.Configuration;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
@@ -17,24 +17,31 @@ public class DirectoryWatcherService extends BackgroundService {
 
     private static final Logger logger = LoggerFactory.getLogger(DirectoryWatcherService.class);
     private final DirectoryWatcherConfig config;
+
     private final Set<Path> seenFiles = ConcurrentHashMap.newKeySet();
 
-   @Inject
-   public DirectoryWatcherService(DirectoryWatcherConfig config) {
-       this.config = config;
-   }
+    @Inject
+    public DirectoryWatcherService(DirectoryWatcherConfig config) {
+        this.config = config;
+    }
 
     @Override
-    protected void workLoop() throws Exception {
+    protected void workLoop() throws IOException {
         try(
-            Stream<Path> paths = Files.list(Path.of(config.getWatchPath()))
-        ){
-            paths.forEach(path -> {
-                if (seenFiles.contains(path.getFileName())){
+                Stream<Path> fileStream = Files.walk(Path.of(config.getWatchPath()));
+        ) {
+            fileStream.forEach(file -> {
+
+                if (file.toString().equals(config.getWatchPath())) {
                     return;
                 }
-                logger.info("Found file {}", path.getFileName());
-                seenFiles.add(path.getFileName());
+                if (seenFiles.contains(file)){
+                   return;
+                }
+
+                logger.info("{}", file.normalize());
+                seenFiles.add(file);
+
             });
         }
     }
