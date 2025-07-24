db37282f5aece1a0029bcbbd463a727f8561ea54
Amrik Singh <amrik.singh@live.co.uk>
2025-07-24 05:42:55 +0100

    rolodex (#545)

--- projects/Rolodex/src/main/java/uk/co/amrik/rolodex/WorkerModule.java
+++ projects/Rolodex/src/main/java/uk/co/amrik/rolodex/WorkerModule.java
@@ -3,14 +3,17 @@ package uk.co.amrik.rolodex;
 import com.google.inject.AbstractModule;
 import com.google.inject.multibindings.Multibinder;
 import uk.co.amrik.rolodex.services.BackgroundService;
+import uk.co.amrik.rolodex.services.watch.DirectoryWatcherModule;
 import uk.co.amrik.rolodex.services.watch.DirectoryWatcherService;
 
 public class WorkerModule extends AbstractModule {
 
     @Override
     protected void configure(){
-        Multibinder<BackgroundService> serviceBinder = Multibinder.newSetBinder(binder(), BackgroundService.class);
 
+        install(new DirectoryWatcherModule());          // Provides DirectoryWatcherConfig
+
+        Multibinder<BackgroundService> serviceBinder = Multibinder.newSetBinder(binder(), BackgroundService.class);
         serviceBinder.addBinding().to(DirectoryWatcherService.class).asEagerSingleton();
     }
 }
--- projects/Rolodex/src/main/java/uk/co/amrik/rolodex/services/watch/DirectoryWatcherService.java
+++ projects/Rolodex/src/main/java/uk/co/amrik/rolodex/services/watch/DirectoryWatcherService.java
@@ -1,9 +1,11 @@
 package uk.co.amrik.rolodex.services.watch;
 
+import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.co.amrik.rolodex.services.BackgroundService;
 
+import java.lang.module.Configuration;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -14,12 +16,19 @@ import java.util.stream.Stream;
 public class DirectoryWatcherService extends BackgroundService {
 
     private static final Logger logger = LoggerFactory.getLogger(DirectoryWatcherService.class);
+    private final DirectoryWatcherConfig config;
     private final Set<Path> seenFiles = ConcurrentHashMap.newKeySet();
-    private int n = 0;
+
+   @Inject
+   public DirectoryWatcherService(DirectoryWatcherConfig config) {
+       this.config = config;
+   }
 
     @Override
     protected void workLoop() throws Exception {
-        try(Stream<Path> paths = Files.list(Path.of("/Users/amrik/code/test-dump"))){
+        try(
+            Stream<Path> paths = Files.list(Path.of(config.getWatchPath()))
+        ){
             paths.forEach(path -> {
                 if (seenFiles.contains(path.getFileName())){
                     return;
