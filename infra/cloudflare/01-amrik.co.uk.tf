7f9ea483f9e2256a8c42f2182309877514c25b86
AmrikSD <amrik.singh@live.co.uk>
2026-01-18 01:36:44 +0700

    thai website

--- infra/cloudflare/01-amrik.co.uk.tf
+++ infra/cloudflare/01-amrik.co.uk.tf
@@ -43,3 +43,50 @@ resource "cloudflare_record" "amrik_co_uk" {
   type    = "CNAME"
   content = "amrik-co-uk.pages.dev"
 }
+
+## == Thai == ##
+resource "cloudflare_pages_project" "thai" {
+  account_id        = data.sops_file.cloudflare-secret.data["cloudflare.account_id"]
+  name              = "thai-amrik-co-uk"
+  production_branch = "main"
+
+  build_config {
+    build_command   = ""
+    root_dir        = "ITP1/Pronunciation"
+    destination_dir = "app"
+  }
+
+  source {
+    type = "github"
+    config {
+      owner                         = "AmrikSD"
+      repo_name                     = "thai"
+      production_branch             = "main"
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting    = "custom"
+      preview_branch_includes       = ["dev", "preview"]
+      preview_branch_excludes       = ["main", "prod"]
+      pr_comments_enabled           = true
+    }
+  }
+
+  deployment_configs {
+    production {
+
+    }
+  }
+}
+
+resource "cloudflare_pages_domain" "thai_subdomain" {
+  account_id   = data.sops_file.cloudflare-secret.data["cloudflare.account_id"]
+  project_name = "thai-amrik-co-uk"
+  domain       = "thai.amrik.co.uk"
+}
+
+resource "cloudflare_record" "thai_amrik_co_uk" {
+  zone_id = data.sops_file.cloudflare-secret.data["cloudflare.amrik.co.uk.zone_id"]
+  name    = "thai"
+  type    = "CNAME"
+  content = "thai-amrik-co-uk.pages.dev"
+}
